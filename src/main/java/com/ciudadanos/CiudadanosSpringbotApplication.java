8426cf5b6cbde38c5d40ef4991f8ecd59508f956
unknown <aldoulaguent@gmail.com>
2023-08-31 15:22:21 -0500

    ciudadanosMYSQL

---
+++ src/main/java/com/ciudadanos/CiudadanosSpringbotApplication.java
@@ -0,0 +1,13 @@
+package com.ciudadanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiudadanosSpringbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiudadanosSpringbotApplication.class, args);
+	}
+
+}
